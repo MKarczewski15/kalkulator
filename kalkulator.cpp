e20938b82df0aeefb6388cc1d2f6bcd99b5417a1
MKarczewski15 <marekkarczewski05@gmail.com>
2024-03-03 20:32:16 +0100

    Update kalkulator.cpp
    zrobione klasami (nie działa)

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -2,22 +2,27 @@
 #include <cmath>
 
 using namespace std;
-
-double kalkulator(double a, double b, char c){
-    if (c == '+'){
-        return a + b;
-    } else if (c == '-'){
-        return a - b;
-    } else if (c == '*'){
-        return a * b;
-    } else if (c == '/'){
-        return a / b;
-    } else{
-        cout<<"niepoprawny znak"<<endl;
+class Kalkulator {
+public:
+    static double wynik(double a, double b, char c) {
+        if (c == '+') {
+            return a + b;
+        } else if (c == '-') {
+            return a - b;
+        } else if (c == '*') {
+            return a * b;
+        } else if (c == '/') {
+            if (b != 0) {
+                return a / b;
+            } else {
+                throw invalid_argument("Nie można dzielić przez zero!");
+            }
+        } else {
+            throw invalid_argument("Niepoprawny znak");
+        }
     }
-    return 0;
-}
+};
 
 int main(){
-    cout << kalkulator(10, 5, '*') << endl;
+    cout<<Kalkulator::wynik(10, 5, '*')<<endl;
 }
