63033d127629fda8e8d9c113e4ba3d752a433959
MKarczewski15 <marekkarczewski05@gmail.com>
2024-03-04 14:23:04 +0100

    kalkulator obiektowy

--- kalkulator.js
+++ kalkulator.js
@@ -1,25 +1,58 @@
-class Kalkulator {
-    static wynik(a, b, c) {
-        if (c === '+') {
-            return a + b;
-        } else if (c === '-') {
-            return a - b;
-        } else if (c === '*') {
-            return a * b;
-        } else if (c === '/') {
-            if (b === 0) {
-                console.log("Nie można dzielić przez zero");
-            } else {
-                return a / b;
-            }
+// class Kalkulator {
+//     static wynik(a, b, c) {
+//         if (c === '+') {
+//             return a + b;
+//         } else if (c === '-') {
+//             return a - b;
+//         } else if (c === '*') {
+//             return a * b;
+//         } else if (c === '/') {
+//             if (b === 0) {
+//                 console.log("Nie można dzielić przez zero");
+//             } else {
+//                 return a / b;
+//             }
+//         } else {
+//             console.log("Niepoprawny znak");
+//         }
+//     }
+
+//     static oblicz(a, b, c) {
+//         return this.wynik(a, b, c);
+//     }
+// }
+
+// console.log(Kalkulator.oblicz(231, 0, '/'));
+
+class Kalkulator{
+
+    dodaj(a, b) {
+        return a + b;
+
+    }
+
+    odejmij(a, b){
+        return a - b;
+    }
+
+    pomnóż(a, b){
+        return a * b;
+    }
+
+    podziel(a, b){
+        if(b === 0) {
+            console.log("nie można dzielić przez 0");
         } else {
-            console.log("Niepoprawny znak");
+            return a / b;
         }
     }
 
-    static oblicz(a, b, c) {
-        return this.wynik(a, b, c);
-    }
 }
 
-console.log(Kalkulator.oblicz(231, 0, '/'));
+const kalkulator = new Kalkulator();
+
+console.log(kalkulator.dodaj(4, 5));
+console.log(kalkulator.odejmij(3, 6));
+console.log(kalkulator.pomnóż(3, 7));
+console.log(kalkulator.podziel(10, 2))
+console.log(kalkulator.podziel(12, 0));
