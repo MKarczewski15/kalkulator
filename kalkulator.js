187fdb22159cfd88deecd43df9b4a802c3fbfac5
Marek Karczewski <marekkarczewski05@gmail.com>
2024-03-06 14:55:42 +0100

    zrobione przez switch

--- kalkulator.js
+++ kalkulator.js
@@ -1,81 +1,3 @@
-// class Kalkulator {
-//     static wynik(a, b, c) {
-//         if (c === '+') {
-//             return a + b;
-//         } else if (c === '-') {
-//             return a - b;
-//         } else if (c === '*') {
-//             return a * b;
-//         } else if (c === '/') {
-//             if (b === 0) {
-//                 console.log("Nie można dzielić przez zero");
-//             } else {
-//                 return a / b;
-//             }
-//         } else {
-//             console.log("Niepoprawny znak");
-//         }
-//     }
-
-//     static oblicz(a, b, c) {
-//         return this.wynik(a, b, c);
-//     }
-// }
-
-// console.log(Kalkulator.oblicz(231, 0, '/'));
-
-// class Kalkulator {
-
-//     sum(a, b) {
-//         return a + b;
-//     }
-
-//     subtract(a, b) {
-//         return a - b;
-//     }
-
-//     multiplication(a, b) {
-//         return a * b;
-//     }
-
-//     divide(a, b) {
-//         if(b === 0) {
-//             console.log("nie można dzielić przez 0");
-//         } else {
-//             return a / b;
-//         }
-       
-//     }
-
-//     static countSum(a, b) {
-//         const kalkulator = new Kalkulator();
-//         return kalkulator.sum(a, b);
-//     }
-
-//     static countSubtract(a, b) {
-//         const kalkulator = new Kalkulator();
-//         return kalkulator.subtract(a, b);
-//     }
-
-//     static countMultiplication(a ,b) {
-//         const kalkulator = new Kalkulator();
-//         return kalkulator.multiplication(a, b);
-//     }
-
-//     static countDivide(a, b) {
-//         const kalkulator = new Kalkulator();
-//         return kalkulator.divide(a, b);
-//     }
-
-// }
-
-// console.log(Kalkulator.countSum(4, 5));
-// console.log(Kalkulator.countSubtract(3, 6));
-// console.log(Kalkulator.countMultiplication(3, 7));
-// console.log(Kalkulator.countDivide(10, 2));
-// //console.log(
-//     const wynik = Kalkulator.countDivide(12, 0);
-
 class Calculator {
     sum(a, b) {
         return a + b;
@@ -116,22 +38,37 @@ ipolicz.addEventListener('click', function (e){
     const vpierwsza = parseFloat(ipierwsza.value);
     const vznak = iznak.value;
     const vdruga = parseFloat(idruga.value);
-    const vwynik = iwynik.value;
     console.log({
         vpierwsza,
         vznak,
         vdruga
     })
 
-    if (vznak === "sum") {
-        iwynik.value = kalkulator['sum'](vpierwsza, vdruga);
-    } else if (vznak === "subtract") {
-        iwynik.value = kalkulator['subtract'](vpierwsza, vdruga);
-    } else if (vznak === "multiplication"){
-        iwynik.value = kalkulator['multiplication'](vpierwsza, vdruga);
-    } else {
-        iwynik.value = kalkulator['divide'](vpierwsza,vdruga);
+    switch (vznak){
+        case "sum":
+            iwynik.value = kalkulator['sum'](vpierwsza, vdruga);
+            break;
+        case "subtract":
+            iwynik.value = kalkulator['subtract'](vpierwsza, vdruga);
+            break;
+        case "multiplication":
+            iwynik.value = kalkulator['multiplication'](vpierwsza, vdruga);
+            break;
+        case "divide":
+            iwynik.value = kalkulator['divide'](vpierwsza,vdruga);
+            break;
     }
+
+
+    // if (vznak === "sum") {
+    //     iwynik.value = kalkulator['sum'](vpierwsza, vdruga);
+    // } else if (vznak === "subtract") {
+    //     iwynik.value = kalkulator['subtract'](vpierwsza, vdruga);
+    // } else if (vznak === "multiplication"){
+    //     iwynik.value = kalkulator['multiplication'](vpierwsza, vdruga);
+    // } else {
+    //     iwynik.value = kalkulator['divide'](vpierwsza,vdruga);
+    // }
    
     return false;
 })
